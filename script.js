332872893a5ec8cff1f56817830a09c83c18965e
julianabritocordeiro <167339581+julianabritocordeiro@users.noreply.github.com>
2025-06-18 17:01:40 -0300

    Aula 6

--- script.js
+++ script.js
@@ -61,6 +61,7 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta() {
     perguntaAtual = perguntas[atual];
@@ -74,8 +75,14 @@ function mostraAlternativas() {
     for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa)
         caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
-
+function respostaSelecionada(opcaoSelecionada){
+        const afirmacoes = opcaoSelecionada.afirmacoes;
+        historiaFinal = afirmacoes;
+        atual++;
+        mostraPergunta();
+}
 mostraPergunta();
